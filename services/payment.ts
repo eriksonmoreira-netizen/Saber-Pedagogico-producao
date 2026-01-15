51cf08557dae37fe61458b14296a2285896a4d80
Saber Pedagógico <erikson.moreira@gmail.com>
2026-01-15 20:43:49 -0300

    Delete services directory

--- services/payment.ts
+++
@@ -1,7 +0,0 @@
-
-/**
- * ARQUIVO DEPRECIADO - ESTRATÉGIA 100% SERVER-SIDE
- * A lógica de integração com o Mercado Pago reside agora exclusivamente no servidor Node.js
- * para garantir segurança e compatibilidade.
- */
-export {};
